5f1a9095202de7430e275b0dac0dd1b0e2c9e820
NazrulIslam-05 <niridoy443@gmail.com>
2023-11-02 22:49:05 +0600

    implement navbar and footer

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import Footer from "./SharedPage/Footer";
+
+
+const Home = () => {
+    return (
+        <div>
+            <h1> This is Home page </h1>
+
+            <Footer/>
+        </div>
+    );
+};
+
+export default Home;
